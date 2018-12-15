513f21b3c06babfb7004cb666d4081b6d0ead5d2
Orilow <ilyaorlov1998@gmail.com>
2018-12-15 19:53:26 +0500

    ngStyle to customize component (here we even use a function)

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent{
-    serverId: number = 10;
-    serverStatus: string = 'offline';
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
+
+    constructor() {
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+      return this.serverStatus === 'offline' ? 'red' : 'green';
     }
 }
